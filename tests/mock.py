c371c6592e7421a5044c1635e74b11fe4fe7f425
Viktor Svekolkin <vsvekolkin@parseq.pro>
2015-10-02 11:24:00 +0300

    Added content types dict.

--- tests/mock.py
+++ tests/mock.py
@@ -74,12 +74,21 @@ class TorrentSuiteMock(MockResource):
             '/auth/output/Home/Run_11_hg19_v3_008/plugin_out/variantCaller_out[\.\d+]*/IonXpress_00\d+/*.vcf': 'mock_vcf.vcf'
         }
         content_types_dict = {
-
+            '.json': 'application/json',
+            '.bam': 'application/bam',
+            '.bed': 'application/bed',
+            '.vcf': 'application/vcf'
         }
-        for uri, json_file_name in uri_dict.items():
-            with open(os.path.join(self.content_dir, json_file_name), 'rb') as f:
+        for uri, file_name in uri_dict.items():
+            _, file_extension = os.path.splitext(file_name)
+            if file_extension in content_types_dict:
+                content_type = content_types_dict[file_extension]
+            else:
+                content_type = ''
+            with open(os.path.join(self.content_dir, file_name), 'rb') as f:
                 httpretty.register_uri(httpretty.GET,
                                        re.compile(self.mock_url + uri),
                                        body=f.read(),
                                        status=200,
+                                       content_type=content_type,
                                        match_querystring=True)
