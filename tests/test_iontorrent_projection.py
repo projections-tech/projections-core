fce28d9cfee6d28df9b6bff0dc4c583c47a93750
Viktor Svekolkin <vsvekolkin@parseq.pro>
2015-10-07 10:24:54 +0300

    Added metadata and BED files check.

--- tests/test_iontorrent_projection.py
+++ tests/test_iontorrent_projection.py
@@ -32,41 +32,41 @@ class TestIonTorrentProjection(TestCase):
         self.iontorrent = iontorrent.IonTorrentProjection(self.mock_url, USER, PASSWORD)
         self.iontorrent.create_projections()
 
-    def test_bed_file_projections_creation(self):
-        """
-        Test BED file projection creation
-        """
-        bed_files_list = [p for p in self.iontorrent.projections if os.path.splitext(p)[1] == '.bed']
-        expected_bed_file_path = os.path.join(self.expected_experiment_path,
-                                              self.expected_run_name,
-                                              'IAD39777_BED_4_for_TSVC.bed')
-
-        self.assertTrue(expected_bed_file_path in bed_files_list)
-
     def test_full_projection(self):
         projection_paths_list = self.iontorrent.projections.keys()
-        exp_dirs = ['test_experiment_1', 'test_experiment_2']
+        exp_dirs = ['/test_experiment_1', '/test_experiment_2']
         run_name = 'test_run'
+
         # Checking number of created projections, we expect 372 projections for two experiments with 5 samples and variant_calling
         self.assertEqual(len(self.iontorrent.projections), 372, msg='Checking projections quantity.')
 
         for exp_dir in exp_dirs:
+            # Checking metadata projection creation for experiments
+            for meta_name in ['metadata.json', 'plannedexperiment.json']:
+                meta_data_path = os.path.join(exp_dir, meta_name)
+                self.assertTrue(meta_data_path in projection_paths_list, msg='Checking metadata projections for experiment {0}.'.format(exp_dir))
+
+            # Checking BED file projections creation
+            bed_file_path = os.path.join(exp_dir, run_name,'IAD39777_BED_4_for_TSVC.bed')
+            self.assertTrue(bed_file_path in projection_paths_list, msg='Checking BED file projection existance: {0}.'.format(bed_file_path))
+
             # Checking BAM file projections creation on expected paths
             for i in range(1,6):
-                bam_file_path = os.path.join('/', exp_dir, run_name, 'sample_{0}'.format(i), 'sample_{0}.bam'.format(i))
+                bam_file_path = os.path.join(exp_dir, run_name, 'sample_{0}'.format(i), 'sample_{0}.bam'.format(i))
                 self.assertTrue(bam_file_path in projection_paths_list, msg='Checking BAM projection existance: {0}'.format(bam_file_path))
+
                 # Checking VCF files projection creation on expected path
                 for variant_caller_dir in ['', '.49', '.50']:
                     for variant_file_name in ['TSVC_variants.vcf', 'all.merged.vcf', 'indel_assembly.vcf',
                                                               'indel_variants.vcf', 'small_variants.left.vcf',
                                                               'small_variants.vcf', 'small_variants_filtered.vcf',
                                                               'small_variants.sorted.vcf', 'SNP_variants.vcf']:
-                        vcf_file_path = os.path.join('/', exp_dir,
+                        vcf_file_path = os.path.join(exp_dir,
                                                      run_name,
                                                      'sample_{}'.format(i),
                                                      'variantCaller_out{}'.format(variant_caller_dir),
                                                      variant_file_name)
                         self.assertTrue(vcf_file_path in projection_paths_list, msg='VCF path: {0}'.format(vcf_file_path))
-        # Checking VCF files projections creation on expected paths
+
 
 
