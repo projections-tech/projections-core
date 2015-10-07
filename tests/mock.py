c6077e029767ea274acd001ca8d80f5a8784a2d1
Anton Bragin <a.g.bragin@gmail.com>
2015-10-07 17:37:49 +0300

    Minor style corrections.
    - mock_auth_response method implementation moved from base class to TorrentSuiteMock.
    - virtualenv directory added to .gitignore file.

--- tests/mock.py
+++ tests/mock.py
@@ -42,12 +42,11 @@ class MockResource(object):
         """
         Reply to basic authorization call, should be overridden in resource-specific manner
         """
-        uri = 'http://{}/rundb/api/v1/'.format(self.mock_url)
-        httpretty.register_uri(httpretty.GET, uri=uri, status=200)
+        pass
 
     def prepare_responses(self):
         """
-        This method should be overridden in resource-specific manner
+        Prepare responses for resource specific set of URI's. Should be overridden in resource-specific manner
         """
         pass
 
@@ -57,21 +56,36 @@ class MockResource(object):
 
 
 class TorrentSuiteMock(MockResource):
+
+    def mock_auth_response(self):
+        uri = 'http://{}/rundb/api/v1/'.format(self.mock_url)
+        httpretty.register_uri(httpretty.GET, uri=uri, status=200)
+
     def prepare_responses(self):
         """
         Prepares mock Torrent Suite replies to requests
         """
         uri_dict = {
-            '/rundb/api/v1/experiment\?status=run\&limit=1\&order_by=-id': 'experiments_metadata_28.json',
-            '/rundb/api/v1/experiment/(\d+)': 'experiments_metadata_28.json',
-            '/rundb/api/v1/plannedexperiment/(\d+)/': 'plannedexperiment_metadata_31.json',
-            '/rundb/api/v1/results/(\d+)/': 'results_8.json',
-            '/rundb/api/v1/pluginresult\?result\=(\d+)': 'plugin_result.json',
-            '/rundb/api/v1/sample/(\d+)/': 'plugin_result.json',
-            '/auth/output/Home/Run_11_hg19_v3_008/plugin_out/variantCaller_out[\.\d+]*/local_parameters.json': 'mock_vc_parameters.json',
-            '/auth/output/Home/Run_11_hg19_v3_008/IonXpress_00\d+_rawlib.bam': 'mock_bam.bam',
-            '/auth/output/Home/Run_11_hg19_v3_008/plugin_out/variantCaller_out[\.\d+]*/IAD39777_BED_4_for_TSVC.bed': 'mock_bed.bed',
-            '/auth/output/Home/Run_11_hg19_v3_008/plugin_out/variantCaller_out[\.\d+]*/IonXpress_00\d+/*.vcf': 'mock_vcf.vcf'
+            '/rundb/api/v1/experiment\?status=run\&limit=1\&order_by=-id':
+                'experiments_metadata_28.json',
+            '/rundb/api/v1/experiment/(\d+)':
+                'experiments_metadata_28.json',
+            '/rundb/api/v1/plannedexperiment/(\d+)/':
+                'plannedexperiment_metadata_31.json',
+            '/rundb/api/v1/results/(\d+)/':
+                'results_8.json',
+            '/rundb/api/v1/pluginresult\?result\=(\d+)':
+                'plugin_result.json',
+            '/rundb/api/v1/sample/(\d+)/':
+                'plugin_result.json',
+            '/auth/output/Home/Run_11_hg19_v3_008/plugin_out/variantCaller_out[\.\d+]*/local_parameters.json':
+                'mock_vc_parameters.json',
+            '/auth/output/Home/Run_11_hg19_v3_008/IonXpress_00\d+_rawlib.bam':
+                'mock_bam.bam',
+            '/auth/output/Home/Run_11_hg19_v3_008/plugin_out/variantCaller_out[\.\d+]*/IAD39777_BED_4_for_TSVC.bed':
+                'mock_bed.bed',
+            '/auth/output/Home/Run_11_hg19_v3_008/plugin_out/variantCaller_out[\.\d+]*/IonXpress_00\d+/*.vcf':
+                'mock_vcf.vcf'
         }
         content_types_dict = {
             '.json': 'application/json',
