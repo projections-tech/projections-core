7747be704fc347d22aaacc8ae7d44e7dc58e629c
Viktor Svekolkin <vsvekolkin@parseq.pro>
2015-10-15 10:55:42 +0300

    Moved Tree tests in test projections, added tests for PrototypeDeserializer, added test_projection_config.yaml file.

--- tests/test_projections.py
+++ tests/test_projections.py
@@ -9,11 +9,11 @@ import json
 
 from unittest import TestCase, skip
 
-from projections import ProjectionPrototype, Projector, Projection, ProjectionTree, ProjectionDriver
+from projections import Tree, ProjectionPrototype, Projector, Projection, ProjectionTree, ProjectionDriver, PrototypeDeserializer
 
 # Import logging configuration from the file provided
 logging.config.fileConfig('logging.cfg')
-logger = logging.getLogger('test_projector')
+logger = logging.getLogger('test_projections')
 
 class TestDriver(ProjectionDriver):
     """
@@ -62,7 +62,6 @@ class TestProjector(TestCase):
         """
         Testing projection tree creation with projection prototypes.
 
-        :return:
         """
 
         # Root projection has ho associated prototypes.
@@ -81,15 +80,17 @@ class TestProjector(TestCase):
         bam_prototype.name = "path.split(environment['data'])[1]"
         bam_prototype.uri = "[environment['data']]"
 
-        experiment_prototype.children = [result_prototype]
-        result_prototype.children = [bam_prototype]
+        result_prototype.parent = experiment_prototype
+        experiment_prototype.children[result_prototype.name] = result_prototype
+        bam_prototype.parent = result_prototype
+        result_prototype.children[bam_prototype.name] = bam_prototype
 
         # Create projection tree with the prototypes provided
         projector = Projector(TestDriver())
         projection_tree = ProjectionTree()
         projection_tree.add_projection(root, None)
 
-        projector.create_projection_tree([experiment_prototype], projection_tree=projection_tree, parent_projection=root)
+        projector.create_projection_tree({'/':experiment_prototype}, projection_tree=projection_tree, parent_projection=root)
 
         dir_paths = ['/', '/experiment_0', '/experiment_1', '/experiment_2',
                  '/experiment_1/result_1', '/experiment_1/result_2',
@@ -109,3 +110,129 @@ class TestProjector(TestCase):
             self.assertTrue(file_path in projection_tree.projections, 'Check that projection exists')
             projection = projection_tree.projections[file_path]
             self.assertTrue(projection.type == stat.S_IFREG, 'Check that this is a file projection')
+
+class TestTreeStructure(TestCase):
+    def setUp(self):
+        self.tree = Tree(name='root')
+        for i in range(2):
+            node = Tree(name=i)
+            node.parent = self.tree
+            self.tree.children[node.name] = node
+
+        sub_node = node
+        for i in range(2,6):
+            node = Tree(name=i)
+            node.parent = sub_node
+            sub_node.children[node.name] = node
+
+        self.last_node = node
+
+    def test_find_method(self):
+        """
+        Tests Tree find method
+        """
+        for i in range(6):
+            self.assertTrue(self.tree.find(i, field='name'),
+                            msg='Testing Tree find() method for object with name: {0}'.format(i))
+
+    def test_path_to_node_method(self):
+        """
+        Tests Tree path_to_node_method
+        """
+        # Checking path to root node which is empty
+        self.assertListEqual([], self.tree.path_to_node())
+
+        # Checking path to children of root node
+        for i in range(2):
+            current_node = self.tree.find(i, field='name')
+            self.assertListEqual(['root'],
+                                 [n.name for n in current_node.path_to_node()],
+                                 msg='Checking path to node: {}'.format(i))
+        # Checking path to children for subnode with name '1'
+        for i in range(2, 6):
+            current_node = self.tree.find(i, field='name')
+            self.assertListEqual(['root', 1],
+                                 [n.name for n in current_node.path_to_node()],
+                                 msg='Checking path to node: {}'.format(i))
+
+    def test_node_descendants(self):
+        """
+        Checks node descendants method of Tree
+        """
+        expected_child_lists = [[0, 1], [2, 3, 4, 5]]
+        for children_dict, exp_list in zip((self.tree.children, self.tree.find(1, field='name').children), expected_child_lists):
+            child_list = sorted(children_dict.keys())
+            self.assertListEqual(child_list, exp_list, msg='Checking node descendants method.')
+
+    def test_traverse_preorder_method(self):
+        """
+        Checks order of preorder traversal of Tree
+        """
+        self.assertListEqual(['root']+list(range(6)),
+                             [n.name for n in self.tree.traverse_pre_order()],
+                             msg='Checking traverse order for preorder method.')
+
+    def test_traverse_post_order(self):
+        """
+        Checks order of post order traversal of Tree
+        """
+        traverse_order = [0, 2, 3, 4, 5, 1, 'root']
+        traverse_result = [n.name for n in self.tree.traverse_post_order()]
+        self.assertListEqual(traverse_order, traverse_result,
+                             msg='Checking order of traverse_post_order method: {}'.format(traverse_result))
+
+    def test_traverse_breadth_first(self):
+        """
+        Checks order of breadth first traversal of Tree
+        """
+        traverse_order = ['root', 1, 5, 4, 3, 2, 0]
+        traverse_result = [n.name for n in self.tree.traverse_breadth_first()]
+        self.assertListEqual(traverse_order, traverse_result,
+                             msg='Checking order of traverse_breadth_first: {}'.format(traverse_result))
+
+    def test_find_node_by_path(self):
+        """
+        Checks if node is on path using Tree find_node_by_path method
+        """
+        tree = Tree(name='/')
+        node_1 = Tree(name='experiments')
+        node_1.parent = tree
+        node_2 = Tree(name='results')
+        node_2.parent = node_1
+
+        tree.children[node_1.name] = node_1
+        node_1.children[node_2.name] = node_2
+
+        path = '/experiments/results'
+        node_by_path = tree.find_node_by_path(path)
+        self.assertTrue(node_by_path.name == 'results',
+                        msg='Checking if node: {0} is on path: {1}'.format(node_by_path.name, path))
+
+class TestPrototypeDeserializer(TestCase):
+    def setUp(self):
+        self.deserializer = PrototypeDeserializer('tests/test_projection_config.yaml')
+
+    def test_prototype_deserialization(self):
+        """
+        Tests test_projection_config deserialization in to ProjectionPrototype tree
+        :return:
+        """
+        root_prototype = self.deserializer.prototype_tree
+        # Test if nodes are ProjectionPrototype instances
+        test_pre_order = [n for n in root_prototype.traverse_pre_order()]
+        for n in test_pre_order:
+            self.assertIsInstance(n, ProjectionPrototype,
+                                  msg='Checking if object: {0} is instance of ProjectionPrototype'.format(root_prototype))
+        # Test correctness of "name" fields of nodes
+        expected_names = ['root_dir', 'results_dir', 'test_bam.bam', 'test_vcf.vcf']
+        test_pre_order_names = [n.name for n in root_prototype.traverse_pre_order()]
+        for element in expected_names:
+            self.assertIn(element, test_pre_order_names,
+                          msg='Checking existance of projection with name {} in a tree.'.format(element))
+        # Test correctness of "uri" fields of nodes
+        expected_uri = ['[object["uri"] for object in environment]', "environment['results']",
+                        "[environment['data_vcf']]", "[environment['data_bam']]"]
+        test_pre_order_uri = [n.uri for n in root_prototype.traverse_pre_order()]
+        for element in expected_uri:
+            self.assertIn(element, test_pre_order_uri,
+                          msg='Checking existance of projection with uri {} in a tree.'.format(element))
