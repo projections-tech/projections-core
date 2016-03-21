f1b02257737a7860f65bdf61464d1b9db86c1bd9
Anton Bragin <abragin@parseq.pro>
2016-03-21 18:48:38 +0300

    Table structure reviewed.

---
+++ sql/projections.sql
@@ -0,0 +1,101 @@
+﻿-- IMPORTANT!: this scripts will delete all data from database if any present!
+
+-- Drop database schema
+DROP SCHEMA IF EXISTS projections CASCADE;
+
+-- Drop and create main database role
+DROP ROLE IF EXISTS projections_admin;
+CREATE ROLE projections_admin;
+
+CREATE SCHEMA projections 
+    AUTHORIZATION projections_admin;
+
+-- TODO: add tables for prototypes data storage
+
+-- Projection tables creation
+CREATE TABLE projections.tree_nodes (
+    node_id bigserial NOT NULL,
+    parent_id serial NOT NULL,
+    node_name varchar,
+    node_path varchar[]
+);
+ALTER TABLE projections.tree_nodes
+    OWNER TO projections_admin;
+
+CREATE TABLE projections.projections (
+    projection_id bigserial PRIMARY KEY,
+    projection_name varchar NOT NULL UNIQUE,
+    mount_point varchar NOT NULL UNIQUE,
+    driver varchar NOT NULL,    -- May reference additional drivers table records
+    projector_pid integer UNIQUE
+);
+ALTER TABLE projections.projections
+    OWNER TO projections_admin;
+
+COMMENT ON TABLE projections_table IS 'Identifies Projections of resources mounted to specific path on the host system.';
+COMMENT ON COLUMN projections_table.projection_name IS 'Human-readable name used to uniquely identify projection.';
+COMMENT ON COLUMN projections_table.mount_path IS 'Absolute projection mount path.';
+COMMENT ON COLUMN projections_table.projector_pid IS 'PID of projection`s projector process.';
+
+-- TODO: harmonize with POSIX filesystem object types
+CREATE TYPE projections.node_types AS ENUM (
+    'FILE',
+    'FOLDER'
+);
+
+CREATE TABLE projections.projection_nodes (
+    LIKE projections.tree_nodes,
+    projection_id bigint NOT NULL,
+    node_content_uri varchar NOT NULL,
+    node_type projections.node_types NOT NULL DEFAULT 'FILE',
+    metadata_content jsonb DEFAULT '{}',
+    PRIMARY KEY (node_id),
+    UNIQUE (projection_id, node_name, node_path),
+    FOREIGN KEY (projection_id)
+        REFERENCES projections.projections (projection_id) MATCH SIMPLE
+        ON UPDATE CASCADE ON DELETE CASCADE
+);
+ALTER TABLE projections.projection_nodes
+    OWNER TO projections_admin;
+CREATE INDEX ON projections.projection_nodes(projection_id);
+-- TODO: Add indexes required for tree operations
+COMMENT ON TABLE tree_table IS 'Holds projection node records each reflecting some data object projected as file or folder.';
+
+CREATE TABLE projections.projection_links (
+    projection_link_id bigserial PRIMARY KEY,
+    head_node_id bigint NOT NULL,
+    tail_node_id bigint NOT NULL,
+    link_name varchar NOT NULL,
+    UNIQUE (head_node_id, tail_node_id, link_name),
+    FOREIGN KEY (head_node_id)
+        REFERENCES projections.projection_nodes (node_id) MATCH SIMPLE
+        ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (tail_node_id)
+        REFERENCES projections.projection_nodes (node_id) MATCH SIMPLE
+        ON UPDATE CASCADE ON DELETE CASCADE
+    -- NOTE: with this design it is in principle possible to link objects from different projections.
+    -- Review this and restrict if needed.
+);
+ALTER TABLE projections.projection_links
+    OWNER TO projections_admin;
+COMMENT ON TABLE projections.projection_links IS 'Holds named and directed links between different projection objects (such as metadata links).';
+
+-- Consider merging this data with projection_nodes since the tables are connected with one-to-one relationship 
+CREATE TABLE projections.projection_node_fs_attributes (
+    projection_node_fs_properties_id bigserial PRIMARY KEY,
+    node_id integer NOT NULL UNIQUE,
+    st_atime int NOT NULL,
+    st_mtime int NOT NULL,
+    st_ctime int NOT NULL,
+    st_size int DEFAULT 1 NOT NULL,
+    st_mode varchar NOT NULL,
+    st_nlink int NOT NULL,
+    st_ino int NOT NULL,
+    FOREIGN KEY (node_id)
+        REFERENCES projections.projection_nodes (node_id) MATCH SIMPLE
+        ON UPDATE CASCADE ON DELETE CASCADE
+);
+ALTER TABLE projections.projection_node_fs_attributes
+    OWNER TO projections_admin;
+
+COMMENT ON TABLE projections.projection_node_fs_attributes IS 'Holds projection node filesystem attributes.';
