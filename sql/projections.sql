38612f7d692616f7116dd6565d0c9f5a5d17cea6
Anton Bragin <abragin@parseq.pro>
2016-03-21 19:53:43 +0300

    Framework for tree functions and corresponding tests established.
    WIP commit.

--- sql/projections.sql
+++ sql/projections.sql
@@ -10,9 +10,7 @@ CREATE ROLE projections_admin;
 CREATE SCHEMA projections 
     AUTHORIZATION projections_admin;
 
--- TODO: add tables for prototypes data storage
-
--- Projection tables creation
+-- Generic nodes table
 CREATE TABLE projections.tree_nodes (
     node_id bigserial NOT NULL,
     parent_id serial NOT NULL,
@@ -22,6 +20,35 @@ CREATE TABLE projections.tree_nodes (
 ALTER TABLE projections.tree_nodes
     OWNER TO projections_admin;
 
+-- TODO: harmonize with POSIX filesystem object types
+CREATE TYPE projections.node_types AS ENUM (
+    'FILE',
+    'FOLDER'
+);
+
+-- Tables for prototypes data storage
+CREATE TABLE projections.prototypes (
+    prototype_id bigserial PRIMARY KEY,
+    prototype_name varchar NOT NULL
+);
+ALTER TABLE projections.prototypes
+    OWNER TO projections_admin;
+
+CREATE TABLE projections.prototype_nodes (
+    LIKE projections.tree_nodes,
+    prototype_id bigserial PRIMARY KEY,
+    prototype_name varchar NOT NULL,
+    node_type projections.node_types NOT NULL DEFAULT 'FILE',
+    uri varchar,
+    -- TODO: consider context retrieval
+    FOREIGN KEY (prototype_id)
+        REFERENCES projections.prototypes (prototype_id) MATCH SIMPLE
+        ON UPDATE CASCADE ON DELETE CASCADE
+);
+ALTER TABLE projections.prototype_nodes
+    OWNER TO projections_admin;  
+
+-- Projection tables
 CREATE TABLE projections.projections (
     projection_id bigserial PRIMARY KEY,
     projection_name varchar NOT NULL UNIQUE,
@@ -36,12 +63,6 @@ COMMENT ON TABLE projections_table IS 'Identifies Projections of resources mount
 COMMENT ON COLUMN projections_table.projection_name IS 'Human-readable name used to uniquely identify projection.';
 COMMENT ON COLUMN projections_table.mount_path IS 'Absolute projection mount path.';
 COMMENT ON COLUMN projections_table.projector_pid IS 'PID of projection`s projector process.';
-
--- TODO: harmonize with POSIX filesystem object types
-CREATE TYPE projections.node_types AS ENUM (
-    'FILE',
-    'FOLDER'
-);
 
 CREATE TABLE projections.projection_nodes (
     LIKE projections.tree_nodes,
@@ -99,3 +120,57 @@ ALTER TABLE projections.projection_node_fs_attributes
     OWNER TO projections_admin;
 
 COMMENT ON TABLE projections.projection_node_fs_attributes IS 'Holds projection node filesystem attributes.';
+
+-- Generic tree functions
+/*
+    Add node
+*/
+CREATE OR REPLACE FUNCTION add_projection_node(
+    projection_id bigint,
+    node_name varchar,
+    parent_node_id bigint,
+    node_content_uri varchar,
+    node_type projections.node_types,
+    metadata_content jsonb
+    ) RETURNS bigint AS
+$BODY$
+DECLARE
+    _node_path varchar[];
+BEGIN
+    -- Create node path from parent node path
+    SELECT COALESCE(array_append(node_path, node_name), ARRAY[])
+    FROM projections.projection_nodes
+    WHERE node_id = parent_node_id
+    INTO _node_path;
+
+    -- TODO: consider update node case
+    INSERT INTO projections.projection_nodes (
+        parent_id,
+        node_name,
+        node_path,
+        projection_id,
+        node_content_uri,
+        node_type,
+        metadata_content
+    ) VALUES (
+        parent_node_id,
+        node_name,
+        _node_path,
+        projection_id,
+        node_content_uri,
+        node_type,
+        metadata_content
+    );
+
+END;
+$BODY$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION add_node() RETURNS SETOF projections.tree_nodes AS
+$BODY$
+DECLARE
+BEGIN
+END;
+$BODY$ LANGUAGE plpgsql;
+
+
+
---
+++ sql/tests/test_projections.sql
@@ -0,0 +1,43 @@
+﻿-- Create pgTAP extension for testing
+CREATE EXTENSION IF NOT EXISTS pgtap;
+
+-- Setup function that can be used to initialize table records before running tests
+CREATE OR REPLACE FUNCTION setup_projections() RETURNS SETOF text AS 
+$$
+BEGIN
+	RETURN;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Setup function that can be used to initialize table records before running tests
+CREATE OR REPLACE FUNCTION test_tree_functions() RETURNS SETOF text AS 
+$BODY$
+DECLARE
+    _projection_id bigint;
+BEGIN
+    RETURN NEXT lives_ok(
+    $$
+        INSERT INTO projections.projections (
+            projection_name,
+            mount_point,
+            driver
+        ) VALUES (
+            'test_projection',
+            '/home/test/',
+            'fs_driver'
+        );
+    $$, 'Create projection');
+
+    -- Get newly created projection ID
+    _projection_id := (
+        SELECT projection_id 
+        FROM projections.projections
+        WHERE projection_name = 'test_projection'
+    );
+
+	RETURN;
+END;
+$BODY$ LANGUAGE plpgsql;
+
+-- Start testing and return the result
+SELECT * FROM runtests();
