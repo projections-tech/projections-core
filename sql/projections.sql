2c8d91f1895c904114e4abc628f833f853dd5245
Anton Bragin <abragin@parseq.pro>
2016-03-23 09:47:49 +0300

    GPLv3 license added to the project.
    SQL files modified to include copyright notice.

--- sql/projections.sql
+++ sql/projections.sql
@@ -1,4 +1,23 @@
-﻿-- IMPORTANT!: this scripts will delete all data from database if any present!
+﻿/*
+    Copyright 2016  Anton Bragin, Victor Svekolkin
+
+    This file is part of Projections.
+
+    Projections is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Projections is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Projections.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+-- IMPORTANT!: this scripts will delete all data from database if any present!
 
 -- Drop database schema
 DROP SCHEMA IF EXISTS projections CASCADE;
--- sql/tests/test_projections.sql
+++ sql/tests/test_projections.sql
@@ -1,4 +1,23 @@
-﻿-- Create pgTAP extension for testing
+﻿/*
+    Copyright 2016  Anton Bragin, Victor Svekolkin
+
+    This file is part of Projections.
+
+    Projections is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Projections is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Projections.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+-- Create pgTAP extension for testing
 CREATE EXTENSION IF NOT EXISTS pgtap;
 
 -- Setup function that can be used to initialize table records before running tests
